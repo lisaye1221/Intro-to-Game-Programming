d3b4c6cd8b560f5527afd08bd781b632d3099319
lisaye1999 <lisaye1999@gmail.com>
2020-09-17 21:56:24 -0400

    added ground, king, fixed movement of player and ball

--- Project 1 - Simple 2D Scene/main.cpp
+++ Project 1 - Simple 2D Scene/main.cpp
@@ -18,16 +18,26 @@ SDL_Window* displayWindow;
 bool gameIsRunning = true;
 GLuint playerTextureID;
 GLuint ballTextureID;
+GLuint kingTextureID;
+GLuint groundTextureID;
 
 ShaderProgram program;
-glm::mat4 viewMatrix, playerMatrix, projectionMatrix, ballMatrix;
+glm::mat4 viewMatrix, playerMatrix, projectionMatrix, ballMatrix, kingMatrix, groundMatrix;
 
 float lastTicks = 0.0f;
 float player_x;
 float ball_x;
 float ball_angle;
-bool goLeft = false;
-bool changeDirection = false;
+float king_size;
+float king_x;
+float king_y;
+bool changedDirection = false;
+float direction = 1.0f;
+float kingScale = 1.0f;
+float king_x_d = 1.0f;
+float king_y_d = -1.0f;
+
+void kingMovementHandler();
 
 
 GLuint LoadTexture(const char* filePath) {
@@ -49,7 +59,7 @@ GLuint LoadTexture(const char* filePath) {
 
 void Initialize() {
     SDL_Init(SDL_INIT_VIDEO);
-    displayWindow = SDL_CreateWindow("Texture", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_OPENGL);
+    displayWindow = SDL_CreateWindow("Let's Roll", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_OPENGL);
     SDL_GLContext context = SDL_GL_CreateContext(displayWindow);
     SDL_GL_MakeCurrent(displayWindow, context);
 
@@ -65,20 +75,17 @@ void Initialize() {
     player_x = 0.0f;
     ball_x = 0.0f;
     ball_angle = 0.0f;
+    king_size = 0.0f;
     // setting up the matrices
     viewMatrix = glm::mat4(1.0f);
     playerMatrix = glm::mat4(1.0f);
     ballMatrix = glm::mat4(1.0f);
+    kingMatrix = glm::mat4(1.0f);
+    groundMatrix = glm::mat4(1.0f);
     projectionMatrix = glm::ortho(-5.0f, 5.0f, -3.75f, 3.75f, -1.0f, 1.0f);
-
-    // set player to its initial position
-    playerMatrix = glm::translate(playerMatrix, glm::vec3(-4.2f, -2.5f, 0.0f));
-    // set ball relative to player(place it next to the player on the right)
-    ballMatrix = glm::translate(playerMatrix, glm::vec3(0.65f, -0.2f, 0.0f));
     
     program.SetProjectionMatrix(projectionMatrix);
     program.SetViewMatrix(viewMatrix);
-    //program.SetColor(1.0f, 0.0f, 0.0f, 1.0f);
 
     glUseProgram(program.programID);
 
@@ -92,6 +99,8 @@ void Initialize() {
     // loading the textures from image files
     playerTextureID = LoadTexture("prince.png");
     ballTextureID = LoadTexture("ball.png");
+    kingTextureID = LoadTexture("king.png");
+    groundTextureID = LoadTexture("ground.png");
 }
 
 void ProcessInput() {
@@ -108,20 +117,15 @@ void Update() {
     // setting model matrix to identity matrix(resetting matrix)
     playerMatrix = glm::mat4(1.0f);
     ballMatrix = glm::mat4(1.0f);
-    
-    if (changeDirection) {
-        // flips the player texture
-        playerMatrix = glm::scale(playerMatrix, glm::vec3(-1.0f, 1.0f, 1.0f));
-        // deals with the deplacement offset
-        playerMatrix = glm::translate(playerMatrix, glm::vec3(-14.2f, -2.5f, 0.0f));
-    }
-    else {
-        // set player to its initial position
-        playerMatrix = glm::translate(playerMatrix, glm::vec3(-4.2f, -2.5f, 0.0f));
-    }
+    kingMatrix = glm::mat4(1.0f);
+    groundMatrix = glm::mat4(1.0f);
+    // placing the textures to their initial position
+    playerMatrix = glm::translate(playerMatrix, glm::vec3(-4.2f, -2.5f, 0.0f));
+    kingMatrix = glm::translate(kingMatrix, glm::vec3(0.0f, 2.8f, 0.0f));
+    groundMatrix = glm::translate(groundMatrix, glm::vec3(0.0f, -3.4f, 0.0f));
+    groundMatrix = glm::scale(groundMatrix, glm::vec3(10.0f, 1.0f, 1.0f));
     // set the ball to the right of player
-    ballMatrix = glm::translate(playerMatrix, glm::vec3(0.65f, -0.2f, 0.0f)); 
-    
+    ballMatrix = glm::translate(playerMatrix, glm::vec3(0.65f * direction, -0.2f, 0.0f)); 
 
     // calculating deltaTime by calculating how much time has gone by since last frame
     float ticks = (float)SDL_GetTicks() / 1000.0f;
@@ -129,35 +133,38 @@ void Update() {
     lastTicks = ticks;
 
     // detect when to change direction
-    if (player_x > 9.0f) {
-        changeDirection = true;
+    if (player_x > 9.7f) { // near the right bound
+        direction = -1;
+    }
+    if (player_x < -1.5f) { // near the left bound
+        direction = 1;
     }
 
-    //if (!goLeft) { // moving to the right
-        player_x += 1.0f * deltaTime;
-        ball_x += 1.0f * deltaTime;
-        ball_angle += -55.0f * deltaTime;
-        playerMatrix = glm::translate(playerMatrix, glm::vec3(player_x, 0.0f, 0.0f));
-        ballMatrix = glm::translate(ballMatrix, glm::vec3(ball_x, 0.0f, 0.0f));
-        ballMatrix = glm::rotate(ballMatrix, glm::radians(ball_angle), glm::vec3(0.0f, 0.0f, 1.0f));
-    //}
-    //else { // moving to the left
-    //    player_x -= 1.0f * deltaTime;
-    //    ball_x -= 1.0f * deltaTime;
-    //    ball_angle += 55.0f * deltaTime;
-    //    playerMatrix = glm::translate(playerMatrix, glm::vec3(player_x, 0.0f, 0.0f));
-    //    ballMatrix = glm::translate(ballMatrix, glm::vec3(ball_x, 0.0f, 0.0f));
-    //    ballMatrix = glm::rotate(ballMatrix, glm::radians(ball_angle), glm::vec3(0.0f, 0.0f, 1.0f));
-    //    
-    //}
+    // set the speed at which player/ball move/rotate
+    player_x += 1.2f * (deltaTime * direction);
+    ball_x += 1.2f * (deltaTime * direction);
+    ball_angle += -65.0f * (deltaTime * direction);
 
-    
+    // adjust the direction at which king moves based on its position
+    kingMovementHandler();
+    // set the speed at which king moves/grows/shrinks
+    king_size += 1.0f * deltaTime * kingScale;
+    king_x += 1.8f * (deltaTime * king_x_d);
+    king_y += 1.8f * (deltaTime * king_y_d);
+    // transform the textures
+    playerMatrix = glm::translate(playerMatrix, glm::vec3(player_x, 0.0f, 0.0f));
+    ballMatrix = glm::translate(ballMatrix, glm::vec3(ball_x, 0.0f, 0.0f));
+    ballMatrix = glm::rotate(ballMatrix, glm::radians(ball_angle), glm::vec3(0.0f, 0.0f, 1.0f));
+    kingMatrix = glm::translate(kingMatrix, glm::vec3(king_x, king_y, 0.0f));
+    kingMatrix = glm::scale(kingMatrix, glm::vec3(king_size, king_size, king_size));
+ 
+    // mirror flipping the player texture when direction changes
+    playerMatrix = glm::scale(playerMatrix, glm::vec3(direction, 1.0f, 1.0f));
 
 }
 
 void Render() {
     glClear(GL_COLOR_BUFFER_BIT);
-
 
     float vertices[] = { -0.5, -0.5, 0.5, -0.5, 0.5, 0.5, -0.5, -0.5, 0.5, 0.5, -0.5, 0.5 };
     float texCoords[] = { 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0 };
@@ -166,14 +173,25 @@ void Render() {
     glVertexAttribPointer(program.texCoordAttribute, 2, GL_FLOAT, false, 0, texCoords);
     glEnableVertexAttribArray(program.texCoordAttribute);
 
+    // draw the ground
+    program.SetModelMatrix(groundMatrix);
+    glBindTexture(GL_TEXTURE_2D, groundTextureID);
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+
+    // draw the player
     program.SetModelMatrix(playerMatrix);
     glBindTexture(GL_TEXTURE_2D, playerTextureID);
     glDrawArrays(GL_TRIANGLES, 0, 6);
 
+    // draw the ball
     program.SetModelMatrix(ballMatrix);
     glBindTexture(GL_TEXTURE_2D, ballTextureID);
     glDrawArrays(GL_TRIANGLES, 0, 6);
 
+    // draw the king
+    program.SetModelMatrix(kingMatrix);
+    glBindTexture(GL_TEXTURE_2D, kingTextureID);
+    glDrawArrays(GL_TRIANGLES, 0, 6);
 
     glDisableVertexAttribArray(program.positionAttribute);
     glDisableVertexAttribArray(program.texCoordAttribute);
@@ -195,4 +213,26 @@ int main(int argc, char* argv[]) {
 
     Shutdown();
     return 0;
+}
+
+// adjust the direction at which king moves based on its position
+void kingMovementHandler() {
+    if (king_size > 2.0f) {
+        kingScale = -1;
+    }
+    if (king_size < 0.7f) {
+        kingScale = 1;
+    }
+    if (king_y < -2.0f) {
+        king_y_d = 1;
+    }
+    if (king_y > 0.5f) {
+        king_y_d = -1;
+    }
+    if (king_x > 4.5f) {
+        king_x_d = -1;
+    }
+    if (king_x < -4.5f) {
+        king_x_d = 1;
+    }
 }
